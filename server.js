3fc4afba59f3303e78a657f5e9e02f1c04408440
Devoxin <lukel9914@gmail.com>
2019-05-29 01:18:29 +0100

    Repurpose for use with Imgen Client

--- server.js
+++ server.js
@@ -1,54 +1,61 @@
 const express = require('express');
-const fs      = require('fs');
-const crypto  = require('crypto');
+const { writeFile } = require('fs');
+const crypto = require('crypto');
+const r = require('rethinkdbdash')({ db: 'imgen' });
 
 /* Express Middleware */
-const sdomain = require('express-subdomain');
-const multer  = require('multer');
-const upload  = multer();
+const upload = require('multer')();
 
 /* Routers/Handlers */
-const app     = express();
-const imgsub  = express.Router();
+const app = express();
 
 /* Config */
-const rootdir = __dirname;
-const port    = 80;
+const currDir = __dirname;
+const port = 80;
 
-/* Server Routes */
+const supportedFormats = ['png', 'bmp', 'webp', 'gif', 'jpg', 'jpeg'];
+const defaultFormat = 'png';
+
+app.use('/*', express.static('i'));
+
 app.post('/upload', upload.any(), async (req, res) => {
-    if (!req.headers['authorization'] || !getKeys().includes(req.headers['authorization']))
-        return res.status(401).json({ error: 'Unauthorized' });
+  if (!req.headers.authorization) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
 
-    const type = req.files[0].originalname.split('.')[1] || 'png';
-    const path = `${crypto.randomBytes(5).toString('hex')}.${type}`;
-    fs.writeFile(`${rootdir}/i/${path}`, req.files[0].buffer, (err) => {
-        if (err)
-            return res.status(500).json({ error: 'Internal error occurred while writing the image data' });
-        
-        return res.status(200).json({ path });
-    })
+  const auth = req.header.authorization || '';
+  const keyExists = await r.table('keys').get(auth).coerceTo('bool');
+
+  if (!keyExists) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const originalFormat = req.files[0].originalname.split('.').pop() || defaultFormat;
+
+  if (!supportedFormats.includes(originalFormat)) {
+    return res.status(400).json({ error: 'Unsupported file format' });
+  }
+
+  const fileName = `${crypto.randomBytes(5).toString('hex')  }.${  originalFormat}`;
+
+  writeFile(`${currDir}/i/${fileName}`, req.files[0].buffer, (err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal error occurred while writing the image data' });
+    }
+
+    res.json({ path });
+  });
 });
 
-imgsub.get('/*', (req, res) => {
-    fs.access(`${rootdir}/i/${req.path}`, fs.constants.R_OK, (err) => {
-        if (err)
-            return notFound(req, res);
-        res.sendFile(`${rootdir}/i/${req.path}`);
-    });
+app.use((_, res, __) => {
+  res.status(404).json({ error: 'Resource not found' });
 });
 
-app.use(sdomain('i', imgsub))
-app.use(notFound)
-app.listen(port, () => console.log('Express Image-Server online'));
+app.listen(port, () => {
+  console.log('Express Image-Server online');
+});
 
-/* Custom Functions */
-function getKeys() {
-    const keys = require('./keys');
-    delete require.cache[require.resolve('./keys')];
-    return keys;
-}
-
-function notFound(req, res, next) {
-    res.status(404).json({ error: 'endpoint/file not found' });
-}
+/**
+ * TODO:
+ *   • 1 hour file sweeping
+ */
